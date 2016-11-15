3ca0b241c200ae88350e15a92a7721330226230e
Luiz Américo <luizamericop@gmail.com>
2016-11-15 16:41:20 -0300

    Redirect to contacts route when transitioning to "/" path

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import { createRouter, middleware} from 'marionette.routing';
 import ApplicationRoute from './application/route';
 import ContactsRoute from './contacts/route';
 import Mn from 'backbone.marionette';
+import Radio from 'backbone.radio';
 
 let router = createRouter({log: true, logError: true});
 
 router.map(function (route) {
-  route('application', {path: '/', abstract: true, routeClass: ApplicationRoute}, function () {
+  route('application', {path: '/', routeClass: ApplicationRoute}, function () {
     route('contacts', {routeClass: ContactsRoute}, function () {
       route('contactdetail', {path: ':contactid'})
     })
@@ -16,5 +17,11 @@ router.map(function (route) {
 router.rootRegion = new Mn.Region({el: '#app'});
 
 router.use(middleware);
+
+Radio.channel('router').on('before:transition', function (transition) {
+  if (transition.path === '/') {
+    transition.redirectTo('contacts')
+  }
+});
 
 router.listen();
