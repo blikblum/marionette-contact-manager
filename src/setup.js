6a92096d3d6ad246f39e347fd04a354e0a77bb53
Luiz Américo <brandkarl@gmail.com>
2017-02-28 08:58:21 -0300

    Add support for Backbone Debugger

--- src/setup.js
+++ src/setup.js
@@ -1,6 +1,12 @@
 import Backbone from 'backbone';
 import Marionette from 'backbone.marionette';
 
+// Marionette Inspector
 if (window.__agent) {
   window.__agent.start(Backbone, Marionette);
+}
+
+// Backbone Debugger
+if (window.__backboneAgent) {
+  window.__backboneAgent.handleBackbone(Backbone);
 }
